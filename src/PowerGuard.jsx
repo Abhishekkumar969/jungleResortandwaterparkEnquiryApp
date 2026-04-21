bd8e1b45266695086a7939e8974123b2d2081840
Abhishek kumar <123351889+Abhishekkumar969@users.noreply.github.com>
2026-04-21 08:13:13 -0800

    Delete src directory

--- src/PowerGuard.jsx
+++
@@ -1,8 +0,0 @@
-import AppLoading from "./AppLoading/AppLoading";
-import BlockedPage from "./pages/BlockedPage";
-
-export default function PowerGuard({ power, children }) {
-    if (power === undefined) return <AppLoading />;
-    if (power === false) return <BlockedPage />;
-    return children;
-}
